5794357d5e3530f86f0cf2f6f05c50f2d8799c6c
Geoff George <geoffgeorgethommana@gmail.com>
2023-09-09 19:51:52 +0530

    css

--- src/Components/Layout/Header.jsx
+++ src/Components/Layout/Header.jsx
@@ -25,11 +25,37 @@ const Header = (props) => {
         </Link>
 
         <div className="flex items-center justify-between">
-          <HeaderCartButton displayCart={props.ShowCart} className="mr-2"/>
+          <HeaderCartButton displayCart={props.ShowCart} className="mr-2" />
           {user?.displayName ? (
-            <button  className="text-sm md:border-0 md:rounded-lg px-1 p-1 md:font-bold" onClick={handleSignOut}>Logout</button>
+            <div className="flex items-center">
+            <Link to={'/account'}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-5 h-5 cursor-pointer mx-1"
+                
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                />
+              </svg>
+              </Link>
+              <button
+                className="text-sm md:border-0 md:rounded-lg px-1 p-1 md:font-bold"
+                onClick={handleSignOut}
+              >
+                Logout
+              </button>
+            </div>
           ) : (
-            <Link to="/signin" className="px-2 p-1 font-bold">Sign in</Link>
+            <Link to="/signin" className="px-2 p-1 font-bold">
+              Sign in
+            </Link>
           )}
 
           {/* <div className={classes["main-image"]}>
